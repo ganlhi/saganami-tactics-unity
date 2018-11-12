2fa8ffb7f4506efb6b69dcae45473bb5b70ddfb6
ganlhi <ganlhi@gmail.com>
2018-11-12 18:55:19 +0100

    Command panel selection and camera buttons

--- Saganami Tactics/Assets/Saganami Tactics/Scripts/Play/CommandPanel.cs
+++ Saganami Tactics/Assets/Saganami Tactics/Scripts/Play/CommandPanel.cs
@@ -36,6 +36,38 @@ namespace ST
 
         #endregion Unity callbacks
 
+        #region Public methods
+
+        public void ToggleFocusOnShip()
+        {
+            PlayController.Instance.ToggleFocusOnShip(PlayController.Instance.SelectedShip);
+        }
+
+        public void SelectNextShip()
+        {
+            var ships = PlayController.Instance.GetAllShips();
+            var idx = (ships.IndexOf(PlayController.Instance.SelectedShip) + 1) % ships.Count;
+            var ship = ships[idx];
+
+            PlayController.Instance.SelectShip(ship);
+        }
+
+        public void SelectPreviousShip()
+        {
+            var ships = PlayController.Instance.GetAllShips();
+            var idx = ships.IndexOf(PlayController.Instance.SelectedShip) - 1;
+            if (idx < 0)
+            {
+                idx = ships.Count - 1;
+            }
+
+            var ship = ships[idx];
+
+            PlayController.Instance.SelectShip(ship);
+        }
+
+        #endregion Public methods
+
         #region Private methods
 
         private void UpdateHeader()
